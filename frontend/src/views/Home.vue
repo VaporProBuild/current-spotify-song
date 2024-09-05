74b759d35f6b9851d9a437ceccd4b6f0d419b8b9
VaporProBuild <bradleyschaefer14@gmail.com>
2024-09-05 17:23:27 -0400

    First update

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Hi</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  components: {},
+  data() {
+    return {}
+  },
+  setup() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style scoped></style>
